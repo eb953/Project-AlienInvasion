81ec818c89aa6add9ad651e26adb840020d6a187
Eric Banavong <eric.banavong@gmail.com>
2023-12-17 20:57:48 -0800

    new

--- Images/resize_image.py
+++ Images/resize_image.py
@@ -1,9 +1,9 @@
 from PIL import Image
 
-image = Image.open('/Users/ericbanavong/Project-AlienInvasion/Project-AlienInvasion/Images/IMG_6506.jpeg')
+image = Image.open('/Users/ericbanavong/Project-AlienInvasion/Project-AlienInvasion/Images/Frida1.jpeg')
 
 image.thumbnail((50,50))
 
-image.save('image_thumbnail.jpeg')
+image.save('Frida1_thumbnail.jpeg')
 
 print(image)
