230302e164da2bec3dee0e6be4b7c205a6faca16
Eric Banavong <eric.banavong@gmail.com>
2023-12-21 15:15:50 -0800

    alien

---
+++ alien.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """Create a class to represent a single alien in a fleet """
+
+    def __init__(self, ai_game):
+        """Initialize the alien into its starting position"""
+        super().__init__()
+        self.screen = ai_game.screen
+        
+        #load the alien into the screen
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect() 
+
+        #start the alien at the top left of the screen
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height 
+
+        #store the alien into the horizontal position 
+        self.x = float(self.rect.x)
