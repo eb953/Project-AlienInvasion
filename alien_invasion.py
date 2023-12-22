4b6e8e281b392952574816b95c503e566810dc01
Eric Banavong <eric.banavong@gmail.com>
2023-12-21 16:34:35 -0800

    new

--- alien_invasion.py
+++ alien_invasion.py
@@ -114,19 +114,30 @@ class AlienInvasion:
     def _create_fleet(self):
         """create the fleet of aliens"""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        ##alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2*alien_width)
         number_aliens_x = available_space_x // (2*alien_width) # floor division // divides 2 numbers and drops any remainder, so we'll get an integer number of aliens 
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+
+        #determine the number of rows of aliens that fit on the screen 
+        ship_height = self.ship.rect.height 
+        available_space_y = (self.settings.screen_height - (3*alien_height) - ship_height)
+        number_rows = available_space_y // (2*alien_height)
+
+        #create the full fleet of aliens
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)    
+
       
 
-    def _create_alien(self, alien_number):
+    def _create_alien(self, alien_number, row_number):
         """create an alien and place it in a row"""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size 
         alien.x = alien_width +2*alien_width *alien_number # the screen width is stored in settings.screen_width 
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height +2*alien.rect.height*row_number
         self.aliens.add(alien)
     
 
