947d4d62630bf921fdb0dbbc58827b778b88ef2a
Eric Banavong <eric.banavong@gmail.com>
2023-12-21 16:53:08 -0800

    fleet direction

--- alien_invasion.py
+++ alien_invasion.py
@@ -141,6 +141,19 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height +2*alien.rect.height*row_number
         self.aliens.add(alien)
+    
+    def _check_fleet_edges(self):
+        """respond appropriately if any aliesn have reached an edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._check_fleet_direction()
+                break
+    
+    def _check_fleet_direction(self):
+        """drop the entire fleet and change the fleet's direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1 
 
     def _update_aliens(self):
         """update the positions of all aliens in the fleet"""
